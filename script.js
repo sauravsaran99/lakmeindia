3a9cb5aa59e098039d5dfdcc554f009d97b784f1
Saurav Kumar <66282953+sauravsaran99@users.noreply.github.com>
2021-12-14 14:10:42 +0530

    First submit

---
+++ script.js
@@ -0,0 +1,29 @@
+document.querySelector('.dot1').addEventListener('click', addImage1);
+
+function addImage1() {
+    document.querySelector('#header-img').src = '/image/header2.jpg';
+}
+
+document.querySelector('.dot2').addEventListener('click', addImage2);
+
+function addImage2() {
+    document.querySelector('#header-img').src = '/image/header1.jpg'
+}
+
+document.querySelector('.dot3').addEventListener('click', addImage3);
+
+function addImage3() {
+    document.querySelector('#header-img').src = '/image/header3.jpg'
+}
+
+document.querySelector('.dot4').addEventListener('click', addImage4);
+
+function addImage4() {
+    document.querySelector('#header-img').src = '/image/header4.jpg'
+}
+
+document.querySelector('.dot5').addEventListener('click', addImage5);
+
+function addImage5() {
+    document.querySelector('#header-img').src = '/image/header5.jpg'
+}
