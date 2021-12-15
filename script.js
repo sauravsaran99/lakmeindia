bd3f6df6bfbb8962ae0450a25d2a4276d20a6d64
Saurav Kumar <66282953+sauravsaran99@users.noreply.github.com>
2021-12-15 19:38:04 +0530

    day2,3

---
+++ script.js
@@ -0,0 +1,7 @@
+
+let arr = [{"image": "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/Hero_Bundle-_1_1_500x.jpg?v=1617893040", "name": "Lakme BLUSH AND GLOW SHEET MASK VALUE SET OF 10", "currentPrice": "599", "oldPrice": "1,000", "discoutPrice": "40% off", "isSoldOut": null},
+{"image": "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/1000x1000-hero_1_500x.jpg?v=1620801652", "name": "Lakme 9TO5 VITAMIN C + SKINCARE VALUE SET", "currentPrice": "999", "oldPrice": "1,297", "discoutPrice": "23% off", "isSoldOut": null},
+{"image": "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/img1_3_1_500x.jpg?v=1620910668", "name": "Lakme GREEN TEA ESSENTIALS KIT", "currentPrice": "699", "oldPrice": "847", "discoutPrice": "17% off", "isSoldOut": null},
+{"image": "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/Lakme9to5Primer_MatteLipColorRedLetter_500x.jpg?v=1598243140", "name": "Lakme 9 to 5 Primer + Matte Lip Color", "currentPrice": "300", "oldPrice": "500", "discoutPrice": "40% off", "isSoldOut": null},
+{"image": "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/24544_H-8901030758850_3e09b888-1276-44a2-9665-fa293cbd5d3_500x.jpg?v=1601470864", "name": "Lakme Forever Matte Liquid Lip Color", "currentPrice": "295", "oldPrice": null, "discoutPrice": null, "isSoldOut": null}];
+
