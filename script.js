53bc6fef3e360b37c0af2ab9189b6b354bf7e937
Saurav Kumar <66282953+sauravsaran99@users.noreply.github.com>
2021-12-16 02:50:40 +0530

    Day 3

--- script.js
+++ script.js
@@ -3,5 +3,67 @@ let arr = [{"image": "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/
 {"image": "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/1000x1000-hero_1_500x.jpg?v=1620801652", "name": "Lakme 9TO5 VITAMIN C + SKINCARE VALUE SET", "currentPrice": "999", "oldPrice": "1,297", "discoutPrice": "23% off", "isSoldOut": null},
 {"image": "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/img1_3_1_500x.jpg?v=1620910668", "name": "Lakme GREEN TEA ESSENTIALS KIT", "currentPrice": "699", "oldPrice": "847", "discoutPrice": "17% off", "isSoldOut": null},
 {"image": "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/Lakme9to5Primer_MatteLipColorRedLetter_500x.jpg?v=1598243140", "name": "Lakme 9 to 5 Primer + Matte Lip Color", "currentPrice": "300", "oldPrice": "500", "discoutPrice": "40% off", "isSoldOut": null},
-{"image": "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/24544_H-8901030758850_3e09b888-1276-44a2-9665-fa293cbd5d3_500x.jpg?v=1601470864", "name": "Lakme Forever Matte Liquid Lip Color", "currentPrice": "295", "oldPrice": null, "discoutPrice": null, "isSoldOut": null}];
+{"image": "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/24544_H-8901030758850_3e09b888-1276-44a2-9665-fa293cbd5d3_500x.jpg?v=1601470864", "name": "Lakme Forever Matte Liquid Lip Color", "currentPrice": "295", "oldPrice": null, "discoutPrice": null, "isSoldOut": null},
+{"image": "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/LakmeAbsoluteMatteUltimateLipColorwithArganOilPetalPink_41b560b6-3f0d-47b2-bfd5-075cf7bc0934_500x.jpg?v=1599200805", "name": "Lakme Absolute Matte Ultimate Lip Color with Argan Oil", "currentPrice": "800", "oldPrice": null, "discoutPrice": null, "isSoldOut": null},
+{"image": "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/Blackjack_500x.png?v=1598528925", "name": "Lakme Absolute Gel Stylist Nail Color", "currentPrice": "250", "oldPrice": null, "discoutPrice": null, "isSoldOut": null},
+{"image": "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/M2_500x.jpg?v=1601551735", "name": "Lakme Color Crush Nail Art", "currentPrice": "155", "oldPrice": null, "discoutPrice": null, "isSoldOut": null},
+{"image": "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/Lakme9to5CCComplexionCareCream_9gmBeige_500x.jpg?v=1598101798", "name": "Lakme 9 to 5 CC Complexion Care Cream, 9gm", "currentPrice": "99", "oldPrice": null, "discoutPrice": null, "isSoldOut": null},
+{"image": "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/2-fop-smokin-glam_1_500x.jpg?v=1603894790", "name": "Lakme Absolute Spotlight Eyeshadow Palette", "currentPrice": "597", "oldPrice": "995", "discoutPrice": "40% off", "isSoldOut": null},
+{"image": "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/Pinks-N238_500x.jpg?v=1603185561", "name": "Lakme True Wear Nail Color", "currentPrice": "88", "oldPrice": "110", "discoutPrice": "20% off", "isSoldOut": null},
+{"image": "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/24329_S1-8901030719165_500x.jpg?v=1633765701", "name": "Lakme Insta Eye Liner", "currentPrice": "130", "oldPrice": null, "discoutPrice": null, "isSoldOut": null},
+{"image": "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/24453_S1-8901030742804_500x.jpg?v=1611756792", "name": "Lakme 9 to 5 Primer + Matte Perfect Cover Foundation", "currentPrice": "352", "oldPrice": "475", "discoutPrice": "26% off", "isSoldOut": null},
+{"image": "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/24704_H-8901030776533_500x.jpg?v=1601452154", "name": "Lakme Cushion Matte", "currentPrice": "275", "oldPrice": null, "discoutPrice": null, "isSoldOut": null},
+{"image": "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/Blue-Scape_500x.jpg?v=1628509200", "name": "Lakme 9 to 5 Primer + Gloss Nail Color", "currentPrice": "144", "oldPrice": "180", "discoutPrice": "20% off", "isSoldOut": null},
+{"image": "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/LakmeLipLoveChapstickCherry_500x.jpg?v=1598359759", "name": "Lakme Lip Love Chapstick", "currentPrice": "150", "oldPrice": null, "discoutPrice": null, "isSoldOut": null},
+{"image": "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/red-carnival_500x.jpg?v=1602153688", "name": "Lakme Absolute 3D Lipstick", "currentPrice": "640", "oldPrice": "800", "discoutPrice": "20% off", "isSoldOut": null},
+{"image": "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/LakmeSunExpertUltraMatteSPF40PA_Compact-7g_500x.jpg?v=1598361828", "name": "Lakme Sun Expert Ultra Matte SPF 40 PA+++ Compact - 7 g", "currentPrice": "196", "oldPrice": "230", "discoutPrice": "15% off", "isSoldOut": null},
+{"image": "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/Lakme9to5NaturaleAloeAquaGel_500x.png?v=1598252908", "name": "Lakme 9to5 Naturale Aloe Aqua Gel", "currentPrice": "160", "oldPrice": "200", "discoutPrice": "20% off", "isSoldOut": null},
+{"image": "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/LakmeAbsoluteWhiteIntenseWet_DryCompactGoldenLight_500x.jpg?v=1603787165", "name": "Lakme  Absolute White Intense Wet & Dry Compact", "currentPrice": "742", "oldPrice": "800", "discoutPrice": "7% off", "isSoldOut": null},
+{"image": "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/Shade_PM14_500x.jpg?v=1603456524", "name": "Lakme Enrich Matte Lipstick", "currentPrice": "295", "oldPrice": null, "discoutPrice": null, "isSoldOut": null},
+{"image": "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/LakmeEyeconicCurlingMascara_Black_9ml_500x.jpg?v=1598357798", "name": "Lakme  Eyeconic Curling Mascara, 9 ml", "currentPrice": "From 375", "oldPrice": null, "discoutPrice": null, "isSoldOut": null}];
 
+if(arr.length > 0) {
+    var disable = document.querySelector('#cart-img');
+    var disable2 = document.querySelector('.belowCart');
+    disable2.setAttribute('class', 'notshow')
+    disable.setAttribute('class', 'notshow')
+
+    var active = document.querySelector('.box3');
+    active.setAttribute('class', 'show')
+
+
+let sum = 0;
+
+arr.map(function(elm) {
+    // div1.setAttribute('src', elm.image)
+    var section2 = document.createElement('section');
+section2.setAttribute('id', 'product');
+var div1 = document.createElement('div');
+div1.setAttribute('class', 'productbox div1');
+var div2 = document.createElement('div');
+var div2p = document.createElement('p');
+div2p.textContent = elm.name;
+div2.append(div2p)
+div2.setAttribute('class', 'productbox div2');
+// var div3 = document.createElement('div');
+// div3.setAttribute('class', 'productbox');
+var div4 = document.createElement('div');
+div4.setAttribute('class', 'productbox');
+var div5 = document.createElement('div');
+div5.setAttribute('class', 'productbox');
+div5.textContent = elm.currentPrice;
+sum = elm.currentPrice
+// console.log(typeof(Number(elm.currentPrice)))
+// var div6 = document.createElement('div');
+// div6.setAttribute('class', 'productbox');
+document.querySelector('.box2').append(section2);
+
+
+    section2.append(div1, div2, div4, div5);
+    var imgProduct = document.createElement('img');
+    imgProduct.setAttribute('id', 'imgProduct');
+    imgProduct.setAttribute('src', elm.image);
+    div1.append(imgProduct);
+})
+// document.querySelector('.totalPrice')
+}
