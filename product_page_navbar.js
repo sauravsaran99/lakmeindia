7f3f3df9d8a21085e1fe6a1896f3e75769e415be
Saurav Kumar <66282953+sauravsaran99@users.noreply.github.com>
2021-12-19 15:40:22 +0530

    Day 5

--- product_page_navbar.js
+++ product_page_navbar.js
@@ -1,3 +1,7 @@
+ 
+
+ 
+ 
  document.querySelector("#home").addEventListener("click", function () {
     window.location.href = "index.html"
 })
@@ -6,10 +10,16 @@
 //     window.location.href = "product_page.html"
 // })
 
-// document.querySelector("#cart").addEventListener("click", function () {
-//     window.location.href = "cart.html"
-// })
+
+document.querySelector("#cart").addEventListener("click", function () {
+    document.querySelector('.fixed').classList.add('displays')
+    // document.querySelector('.fixed').classList.remove('fixed')
+})
+
 
 document.querySelector("#signin").addEventListener("click", function () {
     window.location.href = "signin_form.html"
 }) 
+
+
+
