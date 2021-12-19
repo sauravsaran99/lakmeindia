368479c7fd1a7cd4f70c9c42b0ed365d18e87ca8
Manjunath B V <manjubvmtr122333@gmail.com>
2021-12-19 11:32:48 +0530

    updated

--- signup.js
+++ signup.js
@@ -6,8 +6,9 @@ document.querySelector("button").addEventListener("click", signUp)
         var last = document.querySelector("#lastName").value;
         var email = document.querySelector("#email").value;
         var password = document.querySelector("#pass").value;
+        var checkBox =  document.getElementById("checkmark").checked
 
-        if(first == "" || last == "" || email == "" || password ==""){
+        if(first == "" || last == "" || email == "" || password =="" || checkBox == false){
             alert("Please enter all data")
         }else{
             var obj = {
@@ -17,7 +18,7 @@ document.querySelector("button").addEventListener("click", signUp)
             password:password,
             }
             usersData.push(obj);
-            window.location.href = "signIn.html";
+            window.location.href = "signin_form.html";
         }
 
         localStorage.setItem("userDataBase", JSON.stringify(usersData))
