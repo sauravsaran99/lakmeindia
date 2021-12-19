168d46cae27b8cb1df912b3f8abc2d1d5edea091
Arbaz86 <arbaz10042002@gmail.com>
2021-12-19 10:57:00 +0530

    added cart

--- product_page_navbar.js
+++ product_page_navbar.js
@@ -6,9 +6,9 @@
 //     window.location.href = "product_page.html"
 // })
 
-document.querySelector("#cart").addEventListener("click", function () {
-    window.location.href = "cart.html"
-})
+// document.querySelector("#cart").addEventListener("click", function () {
+//     window.location.href = "cart.html"
+// })
 
 document.querySelector("#signin").addEventListener("click", function () {
     window.location.href = "signin_form.html"
--- signin_form.js
+++ signin_form.js
@@ -20,9 +20,6 @@ document.querySelector("#home").addEventListener("click", function() {
 document.querySelector("#pro").addEventListener("click", function() {
     window.location.href = "pro.html"
 })
-document.querySelector("#cart").addEventListener("click", function() {
-    window.location.href = "cart.html"
-})
 document.querySelector("#signin").addEventListener("click", function() {
         window.location.href = "signIn.html"
     })
